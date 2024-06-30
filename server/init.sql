6ce13970f047a36322369c47c632ddba48664165
Arda Sahan <ardambp@outlook.com>
2024-06-30 15:13:00 +0100

    created patients doctors and appointment tables

--- server/init.sql
+++ server/init.sql
@@ -6,13 +6,39 @@ CREATE DATABASE MASS_DB;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('patient', 'doctor')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS patients (
+    patient_id SERIAL PRIMARY KEY,
+    user_id SERIAL FOREIGN KEY REFERENCES users(user_id),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
-    gender VARCHAR(10) NOT NULL CHECK (gender IN ('male', 'female', 'other')),
+    gender VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS doctors (
+    doctor_id SERIAL PRIMARY KEY,
+    user_id SERIAL FOREIGN KEY REFERENCES users(user_id),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    specialisation VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS appointments (
+    appointment_id SERIAL PRIMARY KEY,
+    patient_id SERIAL FOREIGN KEY REFERENCES patients(patient_id),
+    doctor_id SERIAL FOREIGN KEY REFERENCES doctors(doctor_id),
+    appointment_date DATE NOT NULL,
+    reason VARCHAR(255) NOT NULL CHECK (reason IN ('Routine Check-up', 'Follow-up', 'Consultation', 'Emergency')),
+    status VARCHAR(20) NOT NULL CHECK (status IN ('scheduled', 'canceled', 'completed')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
