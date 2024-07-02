f3700360a5430e20a687fb8eeb7549f8dc52ff3d
Arda Sahan <ardambp@outlook.com>
2024-07-02 21:50:57 +0100

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from "./routes/routes";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", routes);
