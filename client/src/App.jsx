63c340e9e1a89578a52d33c0648169a5305d809e
Ozan Gokberk <ozan8@hotmail.co.uk>
2024-07-02 21:03:11 +0100

    Added landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import DoctorPage from "./pages/DoctorPage/DoctorPage";
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import styles from "./LandingPage.module.css";
+import myIcon from "../../assets/Meditation.svg";
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <section className={styles.section}>
+      <div className={styles.center}>
+        <div className={styles.container}>
+          <h1>Better Mind</h1>
+          <p>
+            Welcome to Better Mind, your trusted platform for convenient
+            and confidential virtual consultations. Whether you're seeking
+            therapy, counseling, or mental health support, our network of
+            licensed professionals is here to help you navigate life's
+            challenges. Connect with experienced therapists from the comfort and
+            privacy of your own home. Your well-being is our priority
+          </p>
+          <button><a href="/register">Get Started</a></button>
+        </div>
+        <img src={myIcon} alt="My Icon" />
+      </div>
+    </section>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
